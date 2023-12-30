3ee9bce0912faec77b095b9ae71dcd6e77b8fd81
jeryldev <jeryldev@gmail.com>
2023-12-30 21:24:09 +0800

    additional updates

--- .credo.exs
+++ .credo.exs
@@ -9,7 +9,7 @@
       strict: true,
       color: true,
       checks: [
-        # https://hexdocs.pm/credo/1.4.0/config_file.html
+        # https://hexdocs.pm/credo/1.7.0-rc.2/config_file.html
         {Credo.Check.Readability.ModuleDoc, false},
         {Credo.Check.Consistency.TabsOrSpaces},
         {Credo.Check.Readability.MaxLineLength, priority: :low, max_length: 100},
